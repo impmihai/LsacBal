fd2cf321460fe14a1b73bc8599bcba9af6cbfe3a
impmihai <mihai.lupea@gmail.com>
2018-11-13 22:09:32 +0200

    notification on landing page

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../account.service';
 import { Router } from '@angular/router';
+import { MessagingService } from '../messaging.service';
 
 @Component({
   selector: 'app-landing',
@@ -9,13 +10,18 @@ import { Router } from '@angular/router';
 })
 export class LandingComponent implements OnInit {
 
-  constructor(private accService: AccountService, private router: Router) { }
+  constructor(private accService: AccountService, private router: Router, private _messagingService: MessagingService) { }
 
   ngOnInit() {
+    this._messagingService.receiveMessage();
+    this._messagingService.currentMessage.subscribe(tst => {
+      console.log(tst);
+    });
   }
 
   login() {
     this.accService.doFacebookLogin();
+    
     this.accService.authStateObservable().subscribe(e => {
       
           this.router.navigate(['/swipe']);
